601bb23f03e87a5bc9e57134b2ab37e9723d9d25
MathisWalcaius <mathis.walcarius@sgsintpaulus.eu>
2019-09-20 17:06:57 +0200

    variabelen - Wisselgeld

--- 04-variabelen/Wisselgeld.py
+++ 04-variabelen/Wisselgeld.py
@@ -4,27 +4,27 @@ eurocent =int(input('geef aantal eurocent = '))
 eurocent2 = int(eurocent)
 
 #berekening
-aantal_mundstukken = eurocent // 100
+aantal_muntstukken = eurocent // 100
 eurocent = eurocent % 100
 
-aantal_mundstukken += (eurocent// 50)
+aantal_muntstukken += (eurocent// 50)
 eurocent = eurocent % 50
 
-aantal_mundstukken += (eurocent// 20)
+aantal_muntstukken += (eurocent// 20)
 eurocent = eurocent % 20
 
-aantal_mundstukken += (eurocent// 10)
+aantal_muntstukken += (eurocent// 10)
 eurocent = eurocent % 10
 
-aantal_mundstukken += (eurocent// 5)
+aantal_muntstukken += (eurocent// 5)
 eurocent = eurocent % 5
 
-aantal_mundstukken += (eurocent// 2)
+aantal_muntstukken += (eurocent// 2)
 eurocent = eurocent % 2
 
-aantal_mundstukken += (eurocent// 1)
+aantal_muntstukken += (eurocent// 1)
 eurocent = eurocent % 1
 
 
-print(eurocent2, 'cent kan je omwisselen in ', aantal_mundstukken, 'mundstukken')
+print(eurocent2, 'cent kan je omwisselen in', aantal_muntstukken, 'muntstukken')
 # uitvoer
