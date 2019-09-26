4e0b81eda08ec35d7949f103766c94e2283d7a02
MathisWalcaius <mathis.walcarius@sgsintpaulus.eu>
2019-09-26 15:59:52 +0200

    variabelen - +Degestopteklok_fout

--- 04-variabelen/+Degestopteklok_fout.py
+++ 04-variabelen/+Degestopteklok_fout.py
@@ -1,5 +1,4 @@
 #invoer
-
 vertrekuur1 = int(input(' '))
 vertrekmin1 = int(input(' '))
 
@@ -15,14 +14,41 @@ vertrekmin4 = int(input(' '))
 
 #berekening
 
-rittijd = (((vertrekuur4 - vertrekuur1) * 60 + ( vertrekmin4 - vertrekmin1)) - ((vertrekuur3 - vertrekuur2) * 60 + ( vertrekmin3 - vertrekmin2))) / 2
-bijnacorrectetijdmin = rittijd % 60 + vertrekmin3
-extrauur = bijnacorrectetijdmin // 60
-correctetijduur = rittijd // 60 + vertrekuur3 + extrauur
-correctetijdmin = bijnacorrectetijdmin % 60
+#rittijd = (((vertrekuur4 - vertrekuur1) * 60 + ( vertrekmin4 - vertrekmin1)) - ((vertrekuur3 - vertrekuur2) * 60 + ( vertrekmin3 - vertrekmin2))) / 2
+#bijnacorrectetijdmin = rittijd % 60 + vertrekmin3
+#extrauur = bijnacorrectetijdmin // 60
+#correctetijduur = rittijd // 60 + vertrekuur3 + extrauur
+#correctetijdmin = bijnacorrectetijdmin % 60
 
 #uitvoer
 
-print(int(correctetijduur))
-print(int(correctetijdmin))
-print('rittijd', rittijd)
+#print(int(correctetijduur))
+#print(int(correctetijdmin))
+#print('rittijd', rittijd)
+
+
+totale_tijd = ((vertrekuur4 - vertrekuur1) * 60) + (vertrekmin4 - vertrekmin1)
+tijd_vriendin = (vertrekuur3 - vertrekuur2) * 60 + (vertrekmin3 - vertrekmin2)
+rittijd = (totale_tijd - tijd_vriendin) / 2
+
+tijd_min_hulp = (rittijd + vertrekmin3) // 60
+tijd_min = (rittijd + vertrekmin3) % 60
+tijd_uur = vertrekuur3 + (rittijd // 60) + tijd_min_hulp
+
+
+print(int(tijd_uur))
+print(int(tijd_min))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
