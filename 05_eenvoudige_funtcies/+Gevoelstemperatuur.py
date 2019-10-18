1c1c926a6b27eb7dae5b88617fec90353ac1c145
MathisWalcaius <mathis.walcarius@sgsintpaulus.eu>
2019-10-18 10:40:11 +0200

    Toetsen - Vergelijking_van_Drake

--- 05_eenvoudige_funtcies/+Gevoelstemperatuur.py
+++ 05_eenvoudige_funtcies/+Gevoelstemperatuur.py
@@ -5,8 +5,8 @@ w = float(input(' '))
 
 #berekening
 
-#gevoelstemperatuur= 13.12 + 0.6215 * t + ((0.3965 * t) + (-11.37)) * (3.6 * w)**0.16
-gevoelstemperatuur = 13.12 + 0.6215 * t + (0.3965 * t - 11.37) * pow(3.6 * w, 0.16)
+gevoelstemperatuur= 13.12 + 0.6215 * t + ((0.3965 * t) + (-11.37)) * (3.6 * w)**0.16
+#gevoelstemperatuur = 13.12 + 0.6215 * t + 0.3965 * t * pow(3.6 * w, 0.16) - 11.37 * pow(3.6 * w, 0.16)
 
 #uitvoer
 print(gevoelstemperatuur)
